674880fb44fdb5cd0aa951e651223b9c85a7e708
sajid-sarkar <64013390+sajid-sarkar@users.noreply.github.com>
2020-05-15 04:59:03 +0530

    Update SecurityConfig.java
    .

--- src/main/java/com/employee/config/SecurityConfig.java
+++ src/main/java/com/employee/config/SecurityConfig.java
@@ -1,7 +1,6 @@
 package com.employee.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -12,7 +11,6 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-@EntityScan("com.employee.entity")
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
